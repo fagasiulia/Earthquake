b49f64d44df5bd960e25b5e4c629f692dd4bec36
fagas <fagas.iulia@yahoo.com>
2019-07-02 21:30:53 +0300

    Parsing a JSON response and create objects out of it. Get the magnitude, place and time from the JSON objects and create earthquakes that will be displayed.

--- app/src/main/java/com/example/android/quakereport/EarthquakeActivity.java
+++ app/src/main/java/com/example/android/quakereport/EarthquakeActivity.java
@@ -16,14 +16,7 @@ public class EarthquakeActivity extends AppCompatActivity {
         setContentView(R.layout.earthquake_activity);
 
         // Create a list of some fake earthquakes
-        ArrayList<Earthquake> earthquakes = new ArrayList<>();
-        earthquakes.add(new Earthquake("4.5", "San Francisco", "Oct 19,2012"));
-        earthquakes.add(new Earthquake("5.3", "London", "Mar 10,2013"));
-        earthquakes.add(new Earthquake("2.7", "Tokyo", "Feb 21,2015"));
-        earthquakes.add(new Earthquake("4.3", "Mexico City", "Oct 19,2012"));
-        earthquakes.add(new Earthquake("7.2", "Moscow", "May 3,2015"));
-        earthquakes.add(new Earthquake("6.8", "Rio de Janeiro", "Jan 30,2018"));
-        earthquakes.add(new Earthquake("5.9", "Paris", "Apr 1, 2016"));
+        ArrayList<Earthquake> earthquakes = QueryUtils.extractEarthquakes();
 
         // Create a new adapter that takes as parameter the list of the earthquakes;
         EarthquakeAdapter earthquakeAdapter = new EarthquakeAdapter(this, earthquakes);
